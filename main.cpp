660fc3ddb27be848e48c21aded0a30e1c61897c6
ziyed4 <ziadassem465@gmail.com>
2023-08-08 14:58:08 +0300

    don't look at it

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+#include <sstream>
+
+#define rep(i, a, b) for(int i = a; i < (b); ++i)
+#define trav(a, x) for(auto& a : x)
+#define all(x) x.begin(), x.end()
+#define sz(x) (int)(x).size()
+typedef long long ll;
+//typedef pair<int, int> pii;
+//typedef vector<int> vi;
+
+using namespace std;
+
+
+	
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+
+	cout <<"don't read this";
+
+			
+		return 0 ;	
+}
+
+
+
